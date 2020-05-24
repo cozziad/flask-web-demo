f1ab1deb631bd7d4a7696a11dd75f1d4b548f5e9
Anthony <cozziad@gmail.com>
2020-05-23 20:47:04 -0400

    initial commit

---
+++ flask-web.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect,url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>yo</h1> Hello World - Home page"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+
+@app.route("/<url_link>")
+def catch(url_link):
+    return "Sorry. Page " + url_link + " doesn't exist!"
+
+if __name__ == "__main__":
+    app.run()
