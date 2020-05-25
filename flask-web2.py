3f991ff149a65e3a616873bec712c8eb66e53fdb
Anthony <cozziad@gmail.com>
2020-05-25 15:55:09 -0400

    message flash

--- flask-web2.py
+++ flask-web2.py
@@ -1,7 +1,9 @@
-from flask import Flask, redirect,url_for, render_template,request, session
+from flask import Flask, redirect,url_for, render_template,request, session,flash
+from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "asdfafa43f44rfe"
+app.permanent_session_lifetime = timedelta(days=5)
 
 @app.route("/")
 def slash():
@@ -24,8 +26,13 @@ def login():
     if request.method == "POST":
         user = request.form["nm"]
         session["user"] = user
+        session.permanent = True
+        flash("Login success")
         return redirect(url_for("user"))
     else:
+        if "user" in session:
+            flash("Welcome back") 
+            return redirect(url_for("user"))
         return render_template("login.html")
 
 @app.route("/user")
@@ -39,6 +46,8 @@ def user():
 @app.route("/logout")
 def logout():
     session.pop("user",None)
+    if "user" in session:
+        flash("You have been logged out!","info")
     return redirect(url_for("login"))
 
 @app.route("/<a>")
